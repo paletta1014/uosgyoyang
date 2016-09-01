53804996a51860323a25b25154e7379dd9decfcf
dbw03135 <dbw03135@gmail.com>
2016-09-01 12:29:29 +0000

    what is this?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,4 +10,9 @@ class HomeController < ApplicationController
 
     @subj=doc.xpath('//list')
   end
+  def mungwa
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2016&term=A20&subjectDiv=A01"),nil,'EUC-KR')
+
+    @subj=doc.xpath('//list')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   get '/hurryup' => 'home#hurryup'
+  get '/mungwa' => 'home#mungwa'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
