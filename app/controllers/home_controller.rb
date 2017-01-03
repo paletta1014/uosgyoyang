943098d308d19548dd352dfb1b48092052813dd1
dbw03135 <dbw03135@gmail.com>
2017-01-03 16:45:45 +0000

    2017

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,16 @@
 class HomeController < ApplicationController
   def index
-    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2016&term=A20&subjectDiv=A01"),nil,'EUC-KR')
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A10&subjectDiv=A01"),nil,'EUC-KR')
 
     @subj=doc.xpath('//list')
-
   end
   def hurryup
-    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2016&term=A20&subjectDiv=A01"),nil,'EUC-KR')
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A10&subjectDiv=A01"),nil,'EUC-KR')
 
     @subj=doc.xpath('//list')
   end
   def mungwa
-    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2016&term=A20&subjectDiv=A01"),nil,'EUC-KR')
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A10&subjectDiv=A01"),nil,'EUC-KR')
 
     @subj=doc.xpath('//list')
   end
