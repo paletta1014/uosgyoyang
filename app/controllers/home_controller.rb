02b02ed4edff1197230fd1609a452d14d4ca6d4c
dbw03135 <dbw03135@gmail.com>
2017-02-24 05:32:41 +0000

    major

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,4 +14,9 @@ class HomeController < ApplicationController
 
     @subj=doc.xpath('//list')
   end
+  def major
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrMjTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A10&deptDiv=%20&dept=%20&subDept=%20"),nil,'EUC-KR')
+
+    @subj=doc.xpath('//list')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/all' => 'home#all'
   get '/mungwa' => 'home#mungwa'
+  get '/major' => 'home#major'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
