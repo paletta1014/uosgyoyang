8b45160ef87038b7e4855693b08e42bb682b8577
dbw03135 <dbw03135@gmail.com>
2017-08-25 22:52:35 +0900

    teacher class

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,7 +15,9 @@ class HomeController < ApplicationController
   end
   def major
     doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrMjTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A20&deptDiv=%20&dept=%20&subDept=%20"),nil,'EUC-KR')
-    @subj=doc.xpath('//list')
-  end
+    doc2 = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A20&subjectDiv=A07"),nil,'EUC-KR')
 
+    @subj=doc.xpath('//list')
+    @subj2=doc2.xpath('//list')
+  end
 end
