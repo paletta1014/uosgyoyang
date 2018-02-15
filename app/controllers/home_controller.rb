5a5c76dfd25be4fe6a65cbd1daf7f013f27ce254
dbw03135 <dbw03135@gmail.com>
2018-02-15 20:37:18 +0900

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,20 +2,20 @@ class HomeController < ApplicationController
   require 'open-uri'
 
   def index
-    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A20&subjectDiv=A01"),nil,'EUC-KR')
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2018&term=A10&subjectDiv=A01"),nil,'EUC-KR')
     @subj=doc.xpath('//list')
   end
   def all
-    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A20&subjectDiv=A01"),nil,'EUC-KR')
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2018&term=A10&subjectDiv=A01"),nil,'EUC-KR')
     @subj=doc.xpath('//list')
   end
   def mungwa
-    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A20&subjectDiv=A01"),nil,'EUC-KR')
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2018&term=A10&subjectDiv=A01"),nil,'EUC-KR')
     @subj=doc.xpath('//list')
   end
   def major
-    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrMjTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A20&deptDiv=%20&dept=%20&subDept=%20"),nil,'EUC-KR')
-    doc2 = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2017&term=A20&subjectDiv=A07"),nil,'EUC-KR')
+    doc = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrMjTimeInq.oapi?apiKey=201605329QCU92763&year=2018&term=A10&deptDiv=%20&dept=%20&subDept=%20"),nil,'EUC-KR')
+    doc2 = Nokogiri::XML(open("http://wise.uos.ac.kr/uosdoc/api.ApiUcrCultTimeInq.oapi?apiKey=201605329QCU92763&year=2018&term=A10&subjectDiv=A07"),nil,'EUC-KR')
 
     @subj=doc.xpath('//list')
     @subj2=doc2.xpath('//list')
